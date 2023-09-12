87aedc3051752e0cae35456ee2754c7b098a0aa3
Anmolzixi <Anmolrajputzixisoft@gmail.com>
2023-09-12 19:04:13 +0530

    crud completed

--- controllers/task.controller.js
+++ controllers/task.controller.js
@@ -3,39 +3,105 @@ const connection = require('../connection')
 function addtask(req, res) {
     try {
         const { category_select, brand_logo, task_name, task_description, price } = req.body;
-    
+
         // Perform the database insertion
         connection.query(
-          'INSERT INTO task (category_select, brand_logo, task_name, task_description, price) VALUES (?, ?, ?, ?, ?)',
-          [category_select, brand_logo, task_name, task_description, price],
-          (err, result) => {
-            if (err) {
-              console.error('Error inserting data: ' + err);
-              res.send({ error: 'Error inserting data' ,status:false});
-            } else {
-              console.log('Data inserted successfully.');
-              res.send({ data: result, status: true });
+            'INSERT INTO task (category_select, brand_logo, task_name, task_description, price) VALUES (?, ?, ?, ?, ?)',
+            [category_select, brand_logo, task_name, task_description, price],
+            (err, result) => {
+                if (err) {
+                    console.error('Error inserting data: ' + err);
+                    return res.send({ error: 'Error inserting data', status: false });
+                } else {
+                    console.log('Data inserted successfully.');
+                    return res.send({ data: result, status: true });
+                }
             }
-          }
         );
-      }
-    catch (err) {
-        console.log("err", err);
+    }
+    catch (error) {
+        return res.send({ data: error, status: false })
     }
 }
-
 
 function gettask(req, res) {
     try {
         connection.query("SELECT * FROM `task`", (err, result) => {
             console.log(result);
-            res.send({ data: result, status: true })
+            return res.send({ data: result, status: true })
         })
     }
-    catch (err) {
-        console.log("err", err);
+    catch (error) {
+        return res.send({ data: error, status: false })
     }
 }
-module.exports = { addtask, gettask }
+
+function deletetask(req, res) {
+    try {
+        const { id } = req.body
+        if (!id) {
+            return res.send({ data: "please enter your id", status: false })
+
+        }
+
+        connection.query(`select id  FROM task WHERE id = ${id}`, (err, result) => {
+            console.log(result);
+            if (result.length != 0) {
+                connection.query(`DELETE FROM task WHERE task.id = ${id}`, (err, result1) => {
+                    console.log(result1);
+                    if (result1) {
+                        return res.send({ data: result1, msg: "delete successfully", status: true })
+
+                    }
+
+
+                })
+
+            }
+            else {
+                return res.send({ msg: "not exist id ", status: true })
+            }
+
+
+        })
+
+    } catch (error) {
+        console.log('delete task error ->', error);
+        return res.send({ data: error, status: false })
+    }
+}
+
+function updatetask(req, res) {
+    try {
+        const { id, category_select, brand_logo, task_name, task_description, price } = req.body;
+
+        if (!id) {
+            return res.send({ data: "please enter your id", status: false })
+
+        }
+
+        connection.query(`select id  FROM task WHERE id = ${id}`, (err, result) => {
+            console.log(result);
+            if (result.length != 0) {
+                connection.query('UPDATE task SET category_select=?, brand_logo=?, task_name=?, task_description=?, price=? WHERE id=?',
+                    [category_select, brand_logo, task_name, task_description, price, id], (err, result1) => {
+                        console.log(result1);
+                        if (result1) {
+                            return res.send({ data: result1, msg: "update successfully", status: true })
+
+                        }
+                    })
+            }
+            else {
+                return res.send({ msg: "not exist id ", status: true })
+            }
+        })
+
+    } catch (error) {
+        console.log('update task error ->', error);
+        return res.send({ data: error, status: false })
+    }
+}
+module.exports = { addtask, gettask, deletetask,updatetask }
 
 
--- routes/task.route.js
+++ routes/task.route.js
@@ -4,6 +4,7 @@ const task = require('../controllers/task.controller');
 
 route.post('/add/task', task.addtask);
 route.get('/get/task', task.gettask);
-
+route.post('/delete/task',task.deletetask)
+route.post('/update/task',task.updatetask)
 
 module.exports = route;
